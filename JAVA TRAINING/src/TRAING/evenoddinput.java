55faf1a627ed80ca5b55ace901b6c284898a1c18
ACER <ACER@DESKTOP-AF35PPD>
2022-03-04 15:11:44 +0530

    FIRST COMMIT

---
+++ JAVA TRAINING/src/TRAING/evenoddinput.java
@@ -0,0 +1,17 @@
+package TRAING;
+import java.util.Scanner;
+public class evenoddinput {
+
+	public static void main(String[] args) {
+	int n;
+	System.out.println("ENTER NUMBER");
+	Scanner input =new Scanner(System.in);
+	n=input.nextInt();
+	// TODO Auto-generated method stub
+if(n%2==0)
+{System.out.println("EVEN");
+	}
+else System.out.println("ODD");
+
+}
+}
