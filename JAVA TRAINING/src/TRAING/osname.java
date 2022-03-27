1d7f73c3b864c7a06d8c064cab535cbd2afa0630
ACER <ACER@DESKTOP-AF35PPD>
2022-03-27 13:40:39 +0530

    os name

---
+++ JAVA TRAINING/src/TRAING/osname.java
@@ -0,0 +1,12 @@
+package TRAING;
+
+public class osname {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String os =System.getProperty("os.name");
+String os1 =System.getProperty("sun.arch.data.model");
+System.out.println(os);
+System.out.println(os1);}
+
+}
