420109e1c15a79a2269ba9f7b24d9cbdfeeeed11
ACER <ACER@DESKTOP-AF35PPD>
2022-03-30 00:03:11 +0530

    count number of matching character in the string

--- JAVA TRAINING/src/TRAING/charat.java
+++ JAVA TRAINING/src/TRAING/charat.java
@@ -8,14 +8,19 @@ Scanner aScanner=new Scanner(System.in);
 String s;
 System.out.println("ENTER STRING");
 s=aScanner.next();
-
+char x;
+System.out.println("enter char to serch");
+x=aScanner.next().charAt(0);
 System.out.println(s.charAt(3));
-
+int c=0;
 System.out.println(s.length());
-//for(int i=s.length();i<0;i--)
-//{System.out.print(s.charAt(i));
+for( int i=0;i<s.length();i++)
+{ if(s.charAt(i)==x)
+{c++;
 
-//}
+}
+
 	}
-
+System.out.print("char exits in string "+ c +" times");
+}
 }
