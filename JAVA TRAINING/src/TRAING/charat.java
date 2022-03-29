0e843f10bfe1fe70f9f0f953e55c9cb3097cff49
ACER <ACER@DESKTOP-AF35PPD>
2022-03-29 23:43:30 +0530

    chatAt

---
+++ JAVA TRAINING/src/TRAING/charat.java
@@ -0,0 +1,21 @@
+package TRAING;
+import java.util.*;
+public class charat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner aScanner=new Scanner(System.in);
+String s;
+System.out.println("ENTER STRING");
+s=aScanner.next();
+
+System.out.println(s.charAt(3));
+
+System.out.println(s.length());
+//for(int i=s.length();i<0;i--)
+//{System.out.print(s.charAt(i));
+
+//}
+	}
+
+}
