3317b55c8eae85d6954d420785f5f11d0152e3cc
ACER <ACER@DESKTOP-AF35PPD>
2022-03-31 13:08:08 +0530

    greater number using math max

---
+++ JAVA TRAINING/src/TRAING/mathmax.java
@@ -0,0 +1,15 @@
+package TRAING;
+import java.util.*;
+public class mathmax {
+
+	public static void main(String[] args) {
+	Scanner aScanner=new Scanner(System.in);
+	System.out.println("enter a");
+	int a=aScanner.nextInt();
+	System.out.println("enter b");
+	int b=aScanner.nextInt();// TODO Auto-generated method stub
+	System.out.println("greater Number is "+Math.max(a, b));;
+	
+	}
+
+}
