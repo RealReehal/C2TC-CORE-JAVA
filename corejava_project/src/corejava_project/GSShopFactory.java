894d8c44eaac35571398fdd4769972e5963bbc3c
Acer <Acer@DESKTOP-MHDUL0A>
2022-04-15 14:05:25 +0530

    done

--- corejava_project/src/corejava_project/GSShopFactory.java
+++ corejava_project/src/corejava_project/GSShopFactory.java
@@ -2,19 +2,21 @@ package corejava_project;
 import framework.*;
 public  class GSShopFactory extends ShopFactory {
 	@Override
-	public PrimeAcc getNewPrimeAcc(int accNo,String accNm,float charges,boolean isSalaried) {
-	GSPrimeAcc GsPrime=new GSPrimeAcc(accNo,accNm,charges,isSalaried);
+	public PrimeAcc getNewPrimeAccount(int accNo,String accNm,float charges,boolean isPrime) {
+	GSPrimeAcc GsPrime=new GSPrimeAcc(accNo,accNm,charges,isPrime);
 	System.out.println(GsPrime.toString());
 	return GsPrime;
 	}
 
-	public NormalAcc getNewNormalAcc(int accNo,String accNm,float charges,float deliveryCharges) {
+	public NormalAcc getNewNormalAccount(int accNo,String accNm,float charges,float deliveryCharges) {
 		GSNormalAcc GsNormal=new GSNormalAcc(accNo,accNm,charges,deliveryCharges);
 				System.out.println(GsNormal.toString());
 		
 		return GsNormal;
 	}
-	
 
+	public NormalAcc getNewNormalAccount(int accNo,String accNm,float charges,boolean isPrime) {
+return null;
 
+}
 }
